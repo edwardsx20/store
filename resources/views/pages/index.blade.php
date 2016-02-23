b690743cd5ace100b4a69322f0d3c80ec492f8c4
Eduardo Aguad <aguad.eduardomiguel@gmail.com>
2016-02-23 12:54:45 -0300

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -13,31 +13,6 @@
 
 	<input type="text" name="s" class="form-control center-mid" placeholder="Buscar">
 
-	
-	<div class="proyecto">
-		
-		@foreach($products as $product)
-		<div class="producto">
-			<div class="producto-imagen">
-				<img src="http://auladigital.cl/smstore2/imagenes/miniaturas_carro/{{$product->portada}}" alt="">
-
-				<div class="producto-precio">
-					<a href="" ng-click="addToCart({{$product->id}})">Agregar al carro</a>
-				</div>
-			</div>
-
-			<div class="producto-datos">
-				<div class="titulo-producto">
-					{{ $product->asignatura . ' ' . $product->nivel}}
-				</div>
-
-				<div class="producto-precio">
-					{{$product->precio}}
-				</div>
-			</div>
-		</div>
-		@endforeach
-	</div>
 
 </div>
 
